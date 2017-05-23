57f47041b13b5b6bf4b9d341ff1c6758a309cd17
littlekite <1582905867@sina.cn>
2017-05-23 18:33:04 +0800

    入口文件增加命名空间

--- core/Kite.php
+++ core/Kite.php
@@ -1,9 +1,10 @@
 <?php
+namespace core;
 class Kite{
     
     public static function createWebApplication()
 	{
-		return self::createApplication('Web');
+		return self::createApplication('core\Web');
 	}
 	public static function createApplication($class)
 	{
