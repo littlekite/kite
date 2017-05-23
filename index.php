115c1668711058996f37a63d81e1a333c41ffcf0
littlekite <1582905867@sina.cn>
2017-05-23 14:59:04 +0800

    取消加载配置文件，更换运行入口文件名字

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
 
 define('KITE_START', microtime(true)); //记录启动赶时间
 require __DIR__.'/core/autoload.php'; //加载核心自动载入文件
-$config = __DIR__.'/core/config/main.php'; //加载核心配置文件
-App::createWebApplication($config)->run(); //启动web程序
+Kite::createWebApplication()->run(); //启动web程序
 
 ?>
