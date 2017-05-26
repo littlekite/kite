584447c59b1e65821978da074cc7a282c4ac4db7
littlekite <1582905867@sina.cn>
2017-05-26 12:16:05 +0800

    加入调试代码。统计程序执行时间

--- index.php
+++ index.php
@@ -6,8 +6,10 @@
  * 
 */
 
-define('KITE_START', microtime(true)); //记录启动赶时间
+define('KITE_START', microtime(true)); //记录启动时间
 require __DIR__.'/core/autoload.php'; //加载核心自动载入文件
-core/Kite::createWebApplication()->run(); //启动web程序
-
+core\Kite::createWebApplication('core\Web')->run(); //启动web程序
+define('KITE_END', microtime(true)); //记录终止时间
+$runtime = number_format(KITE_END - KITE_START, 10);
+echo "执行时间：".$runtime;
 ?>
