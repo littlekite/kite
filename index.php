60a905a5b17d3c99ddb532070e1070db048c6f24
littlekite <1582905867@sina.cn>
2017-05-31 14:41:01 +0800

    ..

--- index.php
+++ index.php
@@ -5,8 +5,13 @@
  * noting Wait add
  * 
 */
-
 define('KITE_START', microtime(true)); //记录启动时间
+define('KITE_DEBUG', true); //调试按钮
+define('DS', DIRECTORY_SEPARATOR);
+if(KITE_DEBUG){
+    error_reporting(E_ALL); //报告所有错误
+    ini_set("display_errors", 1); //显示错误
+}
 require __DIR__.'/core/autoload.php'; //加载核心自动载入文件
 core\Kite::createWebApplication('core\Web')->run(); //启动web程序
 define('KITE_END', microtime(true)); //记录终止时间
