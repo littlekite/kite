578f3fb06cdd1d2ba9d6c075c377cd552ec0a8e1
littlekite <1582905867@sina.cn>
2017-06-13 14:24:38 +0800

    测试数据库查询

--- core/project/index/index.php
+++ core/project/index/index.php
@@ -1,13 +1,26 @@
 <?php
 namespace core\project\index;
 use core\Template;
+use core\Db;
 class index{
     public function index(){
+        
         $temp = new Template();
-        $temp->assign('hw','hello world');
+        
+        //基本的模板标签测试 
+        
+        $temp->assign('hw','1');
         $test_array = ['1','2','3','4'];
         $temp->assign('list',$test_array);
+        
+        //读取数据库
+        $res =Db::query("SELECT id, `name`, `password` FROM k_account WHERE `name` = 'yankuan' AND `password` = '123456'");  
+        
+        print_r($res);
         $temp->display('index','index');  
+        
+        
+        
     }	   
 }
 ?>
