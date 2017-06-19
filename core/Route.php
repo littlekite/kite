fb2b385a32c0e3cd59d8ac8c07c24deadec1104d
littlekite <1582905867@sina.cn>
2017-06-19 12:24:33 +0800

    优化路由处理

--- core/Route.php
+++ core/Route.php
@@ -2,11 +2,19 @@
 namespace core;
 class Route{
     public static function check(){
-        if(isset($_SERVER['PATH_INFO'])){
+        if( isset($_SERVER['PATH_INFO']) ){
             $path_info = ltrim($_SERVER['PATH_INFO'], '/');
             $path = explode('/', $path_info);
-            return $path;
+            $path_style = count($path);
+            if($path_style === 1){
+                return array($path[0],'index');
+            } else if($path_style === 2){ //标准模式
+                return $path;
+            } else { //异常 跳转到error 页面
+                return array('index','error');
+            }
         } else {
+            //直接输入域名 默认到index模块下的index方法
             return array('index','index');
         }    
     }
