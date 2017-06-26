89e95f2322bb10c4b79e7b1bbf9d9ce68f9983a3
littlekite <1582905867@sina.cn>
2017-06-26 10:40:01 +0800

    加载公共函数文件

--- index.php
+++ index.php
@@ -13,9 +13,10 @@ if(KITE_DEBUG){
     error_reporting(E_ALL); //报告所有错误
     ini_set("display_errors", 1); //显示错误
 }
-require __DIR__.'/core/autoload.php'; //加载核心自动载入文件
+require __DIR__.'/core/Autoload.php'; //加载核心自动载入文件
+require __DIR__.'/core/Common.php';//加载功能函数文件
 core\Kite::createWebApplication('core\Web')->run(); //启动web程序
 define('KITE_END', microtime(true)); //记录终止时间
 $runtime = number_format(KITE_END - KITE_START, 10);
-echo "执行时间：".$runtime;
+//echo "执行时间：".$runtime;
 ?>
