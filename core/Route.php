41f397389ad9cfe1da78a602ed42bc1f3fde7ff8
littlekite <1582905867@sina.cn>
2017-06-27 10:40:39 +0800

    添加jump跳转方法 用来跳转到对应的URL

--- core/Route.php
+++ core/Route.php
@@ -1,6 +1,7 @@
 <?php
 namespace core;
 class Route{
+    //路由检测
     public static function check(){
         if( isset($_SERVER['PATH_INFO']) ){
             $path_info = ltrim($_SERVER['PATH_INFO'], '/');
@@ -19,6 +20,28 @@ class Route{
             //直接输入域名 默认到index模块下的index方法
             return array('index','index');
         }    
+   }
+   //ssl检测
+   public static function isSsl()
+    {
+        $server = $_SERVER;
+        if (isset($server['HTTPS']) && ('1' == $server['HTTPS'] || 'on' == strtolower($server['HTTPS']))) {
+            return true;
+        } elseif (isset($server['REQUEST_SCHEME']) && 'https' == $server['REQUEST_SCHEME']) {
+            return true;
+        } elseif (isset($server['SERVER_PORT']) && ('443' == $server['SERVER_PORT'])) {
+            return true;
+        } elseif (isset($server['HTTP_X_FORWARDED_PROTO']) && 'https' == $server['HTTP_X_FORWARDED_PROTO']) {
+            return true;
+        }
+        return false;   
     }
+   //跳转
+   public static function jump($url = ''){
+        // 检测域名
+        $url = (self::isSsl() ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'].'/'.$url;
+        header('Location: '.$url);
+        exit;
+   }
 }
 ?>
