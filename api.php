bbb815e8370b469ae2ad8afa7ed5eaa3b1285dc9
littlekite <1582905867@sina.cn>
2017-07-04 12:11:00 +0800

    增加新的访问API接口 用于移动端直接调用

--- api.php
+++ api.php
@@ -1,5 +1,5 @@
 <?php    
-    define('APP_DEBUG', true);
+    define('KITE_DEBUG', true);
     define('SERVER_START_TIME', microtime(true));
     define('SERVER_START_MEM', memory_get_usage());
     define('DS', DIRECTORY_SEPARATOR);
@@ -7,17 +7,21 @@
     define('RUNTIME_PATH', SERVER_PATH . 'runtime' . DS);
     define('CACHE_PATH', RUNTIME_PATH . 'cache' . DS);
     define('LOG_PATH', RUNTIME_PATH . 'log' .DS);
-    require __DIR__.'/core/autoload.php'; //加载核心自动载入文件
+    require __DIR__.'/core/Autoload.php'; //加载核心自动载入文件
     require __DIR__.'/core/Common.php';
+    if(KITE_DEBUG){
+        error_reporting(E_ALL); //报告所有错误
+        ini_set("display_errors", 1); //显示错误
+    }
     $post = input();
     $m = $post['m'];
-    $method_list = require 'core/enum/Method.php';          
+    $method_list = require 'core/enum/Method.php';         
     if (!empty($m)&&!empty($method_list[$m])) {
         $method = $method_list[$m];
         $table = $method['t'];
         $function = $method['a'];
-        require 'model/'.$table.'.php';
-        $class = 'model\\'.$table;
+        require 'core/api/'.$table.'.php';
+        $class = '\\core\\api\\'.$table;
         $model = new $class();
         echo $model->$function();   
     }
