380e31e1b79ad01e82a012ce4bc59f2b3320a95e
littlekite <15829705867@sina.cn>
2017-10-07 10:45:29 +0800

    bug处理

--- core/Web.php
+++ core/Web.php
@@ -3,9 +3,17 @@ namespace core;
 class Web{
     public function run(){
          $route = Route::check();//路由检测
-         $controller = 'core\project\\'.$route[0].'\\'.$route[1];
+         if(count($route)==2){ //默认home模块
+            $controller = 'core\home\\'.$route[0].'\\'.$route[1];
+            $action = $route[1]; 
+         } else if(count($route)==3){
+            $controller = 'core\\'.$route[0].'\\'.$route[1].'\\'.$route[2]; 
+            $action = $route[2];
+         } else {
+            $controller = 'core\home\index\error'; 
+            $action = 'error';
+         }
          $class = new $controller();
-         $action = $route[1];
          $class->$action();
     }
 }
