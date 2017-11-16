df179ada19f83cb1be473e19eeb63ecf12156c89
littlekite <15829705867@sina.cn>
2017-11-16 09:41:38 +0800

    增加API测试路径及方法 提供了一个简单的demo

--- api.php
+++ api.php
@@ -7,28 +7,28 @@
     define('RUNTIME_PATH', SERVER_PATH . 'runtime' . DS);
     define('CACHE_PATH', RUNTIME_PATH . 'cache' . DS);
     define('LOG_PATH', RUNTIME_PATH . '/log/api' .DS);
-    require __DIR__.'/core/Autoload.php'; //加载核心自动载入文件
+    require __DIR__.'/core/Autoload.php';
     require __DIR__.'/core/Common.php';
     $post = input();
-    if(KITE_DEBUG){
-        error_reporting(E_ALL); //报告所有错误
-        ini_set("display_errors", 1); //显示错误
+    if (KITE_DEBUG) {
+        error_reporting(E_ALL);
+        ini_set("display_errors", 1);
         if (!empty($post)) {
             $d_string = json_encode($post); 
         } else {
-            $d_string = "have request but no param";  
+            $d_string = "have request but no params";  
         }
         core\Db::execute("INSERT INTO `k_getdata` (`id`, `data`, `data_url`) VALUES (null, ?, ?)", [$d_string, $_SERVER['REQUEST_URI']]); //记录每一个请求信息
-        $id = core\Db::getId(); 
+        $id = core\Db::getId(); //获取请求记录的ID
     }
     $m = $post['m'];
-    $method_list = require 'core/enum/Method.php';        
-    if (!empty($m)&&!empty($method_list[$m])) {
+    $method_list = require 'project/api/static/Method.php';        
+    if (!empty($m) && !empty($method_list[$m])) {
         $method = $method_list[$m];
         $table = $method['t'];
         $function = $method['a'];
-        require 'core/api/'.$table.'.php';
-        $class = '\\core\\api\\'.$table;
+        require 'project/api/'.$table.'.php';
+        $class = '\\project\\api\\'.$table;
         $model = new $class();
         echo $model->$function();  
         if(KITE_DEBUG){
---
+++ project/api/test.php
@@ -0,0 +1,10 @@
+<?php
+namespace project\api;
+class test{
+    public function test(){
+        
+        echo 1;
+        
+    }
+}
+?>
