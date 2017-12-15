48dfd151a3585377db71e1c87692f9fb87c6fcea
littlekite <15829705867@sina.cn>
2017-12-15 20:23:50 +0800

    逻辑更新

--- project/home/index/index.php
+++ project/home/index/index.php
@@ -8,9 +8,9 @@ class index{
         $temp = new Template();
         //基本的模板标签测试 
         $temp->assign('hw','1');
-        
         //读取数据库
-        $res =Db::query("SELECT id, `name`, `password` FROM k_account");  
+        $res = Db::query("SELECT id, `name`, `password` FROM k_account");
+        var_dump($res);
         $temp->assign('list',$res);
         $temp->display('project/home/index/index');   
     }	   
