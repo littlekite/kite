0f0ffdb844676f070c06ef7190a0be3fb3c2d486
littlekite <15829705867@sina.cn>
2018-09-20 18:24:20 +0800

    修正了一个Linux下类无法定位的问题（大小写和命名规范导致、）

--- core/Kite.php
+++ core/Kite.php
@@ -7,23 +7,23 @@ class Kite{
          if (in_array($route[0], $config['module_list'])){
             //设置模块常量
             define('MOUDEL_NAME', $route[0]);
-            //设置当前模板路径常量
+            //设置当前路径常量
             if (!empty($_SERVER['REQUEST_URI']) && !empty($_SERVER['PATH_INFO'])){
                 $pos = strpos($_SERVER['REQUEST_URI'], $_SERVER['PATH_INFO']);
                 $view_url = substr($_SERVER['REQUEST_URI'], 0, $pos).'/project/'.MOUDEL_NAME.'/view/';
                 define('VIEW_URL', $view_url);
             }
             if (count($route) == 2 && empty($route[1])) { //默认home模块
-                $controller = 'project\\'.$route[0].'\\index';
-                $action = 'index'; 
+                $controller = 'project\\'.$route[0].'\\Index';
+                $action = 'index';
             } elseif (count($route) == 2 && !empty($route[1])) { //默认home模块
-                $controller = 'project\\'.$route[0].'\\'.$route[1];
-                $action = $route[1]; 
+                $controller = 'project\\'.$route[0].'\\'.ucfirst($route[1]);
+                $action = $route[1];
             } elseif (count($route) == 3 && !empty($route[1]) && empty($route[2])){
-                $controller = 'project\\'.$route[0].'\\'. $route[1].'\\index'; 
+                $controller = 'project\\'.$route[0].'\\'. $route[1].'\\Index'; 
                 $action = 'index';
             } elseif (count($route) == 3 && !empty($route[1]) && !empty($route[2])){
-                $controller = 'project\\'.$route[0].'\\'. $route[1].'\\'.$route[2]; 
+                $controller = 'project\\'.$route[0].'\\'. $route[1].'\\'.ucfirst($route[2]); 
                 $action = $route[2];
             } else {
                $controller = 'project\home\error'; 
